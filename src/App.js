8aa20bd348b1707c2c83903dcba0776aa0706ddb
dawitmulugeta1990 <you@example.com>
2020-08-16 13:27:41 -0400

    friend list assignment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./Component/Header";
+import FriendCard from "./Component/FriendCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    list: [
+      {
+        photo: require("./Component/photo-one.jpg"),
+        name: "SpongeBob",
+        occupation: "Fry cook",
+        location: "A pinapple under the sea",
+      },
+      {
+        photo: require("./Component/photo-three.jpg"),
+        name: "Mr. Krabes",
+        occupation: "Restaurant Owner",
+        location: "A Giant Anchor ",
+      },
+      {
+        photo: require("./Component/photo-two.jpg"),
+        name: "Squadward",
+        occupation: "Casher",
+        location: "An Easter Island Head",
+      },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        <Header />
+        <FriendCard friend={this.state.list} />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/Component/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div style={styleHead}>
+      <h1>
+        <u>Friends List</u>
+      </h1>
+    </div>
+  );
+}
+const styleHead = {
+  display: "flex",
+  justifyContent: "center",
+  fontSize: "20px",
+  backgroundColor: "#ffffff",
+  color: "blue",
+};
